6895e310ba8b2821dadb2c5355fe73e79eb4d4d5
Aldo <aldo_roman13@hotmail.com>
2024-12-13 18:56:31 -0600

    Add slideshow-angular-swiper folder

---
+++ slideshow-angular-swiper/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SlidesComponent } from './slides/slides.component';
+
+// app.module.ts
+import { SwiperModule } from 'swiper/angular';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SlidesComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    SwiperModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
